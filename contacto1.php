f0ab05b0c93ed8aab6a974b71457beb2989fe7e8
RubenHJ45 <98754467+RubenHJ45@users.noreply.github.com>
2022-02-04 15:43:44 +0100

    familia de dios 1.2.2

--- contacto1.php
+++ contacto1.php
@@ -1,10 +1,88 @@
+<?php
+
+use PHPMailer\PHPMailer\{PHPMailer, SMTP, Exception};
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+
+if(isset($_POST['submit'])){
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $asunto = $_POST['asunto'];    
+    $mensaje = $_POST['mensaje'];
+
+    $errors = array();
+
+    if(empty($nombre)){
+
+        $errors[] = 'El campo nombre es obligatorio';
+    }
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+
+        $errors[] = 'La direccion de coreo no es valida';
+    }
+    if(empty($asunto)){
+
+        $errors[] = 'El campo asunto es obligatorio';
+    }
+    if(empty($mensaje)){
+
+        $errors[] = 'El campo mensaje es obligatorio';
+    }
+
+    if(count($errors) == 0){
+
+        $msj = "De: $nombre <a href='mailto:$email'>$email</a><br>";
+        $msj .= "Asunto: $asunto<br><br>";
+        $msj .= "Cuerpo del mesaje:";
+        $msj .= '<p>' .$mensaje. '</p>';
+        $msj .= "--<p>Este mensaje se a enviado de un formulario de contacto de Familia de dios.</p>";
+
+        $mail = new PHPMailer(true);
+
+        try{
+
+            $mail->SMTPDebug = SMTP::DEBUG_OFF;
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'contatofamiliadedios@gmail.com';
+            $mail->Password = 'Contacto5733';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $mail->Port = 465;
+
+            $mail->setFrom('contatofamiliadedios@gmail.com');
+            $mail->addAddress('familiadediosf@gmail.com');
+            
+            $mail->isHTML(true);
+            $mail->Subject = 'Formulario de contacto FDD';
+            $mail->Body = $msj;
+
+            $mail->send();
+
+            $respuesta = '!! CORREO ENVIADO !!';
+
+
+
+        }catch(Exception $e){
+            $respuesta = 'Mensaje ' . $mail->ErrorInfo;
+        }
+    }
+}
+
+?>
+
+
+
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Conocenos</title>
+    <title>Formulario Registro</title>
         <!-- font awesome -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
         <!--fuente oswal-->
@@ -13,6 +91,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400&display=swap" rel="stylesheet">
         <!--Estilo css-->
         <link rel="stylesheet" href="estilo.css">
+        <script src="js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
     
@@ -26,9 +105,8 @@
                 <li><a href="biblioteca.html">Testimonios</a></li>
                 <li><a href="conocenos.html">Conocenos</a></li>
                 <li><a href="donativos.html">Donativos</a></li>
-                <li><a href="contacto1.php">Contacto</a></li>  
-                <li><a href="inicioSesion.html">Entrar</a></li>
-                
+                <li><a href="contacto1.php">Contacto</a></li>
+                <li><a href="inicioSesion.html">Entrar</a></li>   
                 <li class="container-submenu">
                     <a href="#" class="submenu-btn">Mas...</a>
                     <ul class="submenu">
@@ -37,7 +115,6 @@
                         <li class="menu-item"><a href="consejeria.html" class="menu-link">Consejeria</a></li>
                         <li class="menu-item"><a href="biblioteca.html" class="menu-link">Biblioteca</a></li>
                         <li class="menu-item"><a href="orientacion.html" class="menu-link">Orientacion</a></li>
-                        <li class="menu-item"><a href="farmularioRegristo.html" class="menu-link">Regristrate</a></li>
                     </ul>    
             </ul>
 
@@ -54,8 +131,7 @@
             </ul> 
             
         </nav>
-        <hr>
-        
+        <hr>        
 
             <div id="ctn-bars-search">
                 <input type="text" id="imputSearch" placeholder="¿Que deseas buscar?">
@@ -75,16 +151,43 @@
 
             <div id="cover-ctn-search"></div>
 
-               <!-- ANUNCIO DE OBRAS -->
+     
 
-               <div class="obras">
-                <h1 id="titulo" style="text-align: center; font-size: 4rem;">Familia de <b style="color: gold;">Dios</b></h1><br><br>
-                <h1 class="info-obras">ESTA SECCION SE ENCUENTRA EN OBRAS</h1>
-                <h3 class="info-obras">LAMENTAMOS LAS MOLESTIAS</h3>
+            
 
+            <form class="form-register"  method="POST" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" autocomplete="off">
+                <div class="form-container">
+                    <h2>Formulario Contacto</h2>
+                    <label for="nombre" class="form-label">Nombre</label>
+                    <input class="controls" type="text" name="nombre" id="nombre" autofocus require>
+
+                    <label for="Asunto" class="form-label">Asunto</label>
+                    <input class="controls" type="text" name="asunto" id="asunto" required>
+
+                    <label for="email" class="form-label">Correo Electronico</label>
+                    <input class="controls" type="email" name="email" id="email" required>
+
+                    <label for="mensaje" class="form-label">Mensaje</label>
+                    <textarea class="controls-text" name="mensaje" id="mensaje" cols="30" rows="5" required></textarea>                
+                    
+                    <input class="botons" type="submit" name="submit" value="Enviar">
+                    <?php if(isset($respuesta)){ ?>
                 
-            </div>
+                <div class="validacion">
+    
+                    <h2 class="correo-enviado"><?php echo $respuesta; ?></h2>
+    
+                </div>
+    
+                    <?php } ?>
+                    
+                    <p><a href="inicioSesion.html">¿Ya tienes Cuenta?</a></p>
+                    <p><a href="farmularioRegristo.html">¿Aun no tienes Cuenta?</a></p>
+                </div>
+            </form>
 
+            
+                
             <section class="social">
                 <p>SIGUENOS EN NUESTRAS REDES</p>
                 <div class="links">
@@ -106,6 +209,7 @@
                 </div>
             </section>
 
+           
         </div>
 
 
@@ -118,4 +222,3 @@
         
 </body>
 
-</html>
